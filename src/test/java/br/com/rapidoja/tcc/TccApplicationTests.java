7554d5c42abdce24e1b1e868d329df8ed46c3519
Pedro Alves <phenrique.alvesilva@gmail.com>
2026-06-21 21:59:17 -0300

    feat: initial files

---
+++ src/test/java/br/com/rapidoja/tcc/TccApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rapidoja.tcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
